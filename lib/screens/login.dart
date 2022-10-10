0abbdb9071a5ccc82c4bae5ffa297ed976a39b4d
lahcenegue <98424839+lahcenegue@users.noreply.github.com>
2022-10-10 22:16:48 +0100

    login page

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,17 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:libraryapp/widgets/login_card.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Directionality(
-        textDirection: TextDirection.rtl,
-        child: SafeArea(
-            child: Scaffold(
-          appBar: AppBar(
-            title: Text('تسجيل الدخول'),
-          ),
-        )));
+    return DefaultTabController(
+      length: 2,
+      child: Directionality(
+          textDirection: TextDirection.rtl,
+          child: SafeArea(
+              child: Scaffold(
+            appBar: AppBar(
+              title: const Text('تسجيل الدخول'),
+              centerTitle: true,
+              bottom: const TabBar(
+                tabs: [
+                  Tab(
+                    text: 'دخول مشتري',
+                  ),
+                  Tab(
+                    text: 'دخول مكتبة',
+                  ),
+                ],
+              ),
+            ),
+            body: const TabBarView(
+              children: [
+                LoginCard(),
+                LoginCard(),
+              ],
+            ),
+          ))),
+    );
   }
 }
---
+++ lib/screens/register.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+import 'package:libraryapp/screens/login.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  bool _obscureText = true;
+  @override
+  Widget build(BuildContext context) {
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return SafeArea(
+      child: Directionality(
+        textDirection: TextDirection.rtl,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('تسجيل جديد'),
+            centerTitle: true,
+          ),
+          body: SizedBox(
+            height: screenHeight,
+            width: screenWidth,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                SingleChildScrollView(
+                  child: Container(
+                    height: screenHeight * 0.60,
+                    width: screenWidth * 0.75,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: Colors.grey,
+                          blurRadius: 4,
+                          offset: Offset(4, 8), // Shadow position
+                        ),
+                      ],
+                      border: Border.all(
+                        width: 2,
+                        color: Colors.white.withOpacity(0.5),
+                      ),
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          //رقم الهوية
+                          SizedBox(
+                            height: 45,
+                            child: TextField(
+                              keyboardType: TextInputType.number,
+                              decoration: InputDecoration(
+                                fillColor: Colors.grey.shade100,
+                                filled: true,
+                                hintText: 'رقم الهوية',
+                                contentPadding: const EdgeInsets.only(
+                                    left: 15, bottom: 11, top: 11, right: 15),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          // تاريخ الميلاد
+                          SizedBox(
+                            height: 45,
+                            child: TextField(
+                              keyboardType: TextInputType.datetime,
+                              decoration: InputDecoration(
+                                fillColor: Colors.grey.shade100,
+                                filled: true,
+                                hintText: 'تاريخ الميلاد',
+                                contentPadding: const EdgeInsets.only(
+                                    left: 15, bottom: 11, top: 11, right: 15),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          //رقم الجوال
+
+                          SizedBox(
+                            height: 45,
+                            child: TextField(
+                              keyboardType: TextInputType.phone,
+                              decoration: InputDecoration(
+                                fillColor: Colors.grey.shade100,
+                                filled: true,
+                                hintText: 'رقم الجوال',
+                                contentPadding: const EdgeInsets.only(
+                                    left: 15, bottom: 11, top: 11, right: 15),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          // الامايل
+                          SizedBox(
+                            height: 45,
+                            child: TextField(
+                              keyboardType: TextInputType.emailAddress,
+                              decoration: InputDecoration(
+                                fillColor: Colors.grey.shade100,
+                                filled: true,
+                                hintText: 'الايميل',
+                                contentPadding: const EdgeInsets.only(
+                                    left: 15, bottom: 11, top: 11, right: 15),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 45,
+                            child: TextField(
+                              keyboardType: TextInputType.visiblePassword,
+                              obscureText: _obscureText,
+                              enableSuggestions: false,
+                              autocorrect: false,
+                              decoration: InputDecoration(
+                                  fillColor: Colors.grey.shade100,
+                                  filled: true,
+                                  hintText: 'كلمة المرور',
+                                  contentPadding: const EdgeInsets.only(
+                                      left: 15, bottom: 11, top: 11, right: 15),
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  suffixIcon: IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          _obscureText = !_obscureText;
+                                        });
+                                      },
+                                      icon: Icon(_obscureText
+                                          ? Icons.visibility_off
+                                          : Icons.visibility))),
+                            ),
+                          ),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize: const Size(double.infinity, 50),
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0)),
+                            ),
+                            onPressed: () async {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => const LoginScreen(),
+                                ),
+                              );
+                            },
+                            child: const Text('تسجيل'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
