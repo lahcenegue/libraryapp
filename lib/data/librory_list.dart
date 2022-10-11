7aba687196133f8ee32030b83455e2ea2748c420
lahcenegue <98424839+lahcenegue@users.noreply.github.com>
2022-10-11 06:35:47 +0100

    library list

---
+++ lib/data/librory_list.dart
@@ -0,0 +1,7 @@
+class LibraryList {
+  List<Map> libraryList = [
+    {'name': 'مكتبة حي الخالدية'},
+    {'name': 'مكتبة حي المحمدية'},
+    {'name': 'مكتبة حي العزيزية'},
+  ];
+}
--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:libraryapp/data/librory_list.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    LibraryList libraryList = LibraryList();
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
     return Directionality(
       textDirection: TextDirection.rtl,
       child: SafeArea(
@@ -12,8 +16,23 @@ class MainScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text('قائمة المكتبات'),
         ),
-        body: ListView(
-          children: [],
+        body: ListView.separated(
+          itemCount: libraryList.libraryList.length,
+          separatorBuilder: (context, index) {
+            return const SizedBox(height: 20);
+          },
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(left: 50, right: 50),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(),
+                onPressed: () {},
+                child: Text(
+                  libraryList.libraryList[index]['name'],
+                ),
+              ),
+            );
+          },
         ),
       )),
     );
